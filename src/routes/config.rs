c4c6a28ffcb14198fd7427b17b45efc0bda60b7f
andisltn48 <andiaril186@gmail.com>
2025-01-27 22:48:35 +0800

    update config

--- src/routes/config.rs
+++ src/routes/config.rs
@@ -1,19 +1,16 @@
 use actix_web::web;
 
 use super::game::{get_games, create_game, delete_game, get_game_by_id, update_game};
-use super::auth::{login};
+use super::auth::login;
 
 pub fn config(conf: &mut web::ServiceConfig) {
-    let game_scope = web::scope("/api")
+    let scope = web::scope("/api")
         .service(get_games)
         .service(create_game)
         .service(delete_game)
         .service(get_game_by_id)
-        .service(update_game);
-
-    let auth_scope = web::scope("/api")
+        .service(update_game)
         .service(login);
 
-    conf.service(game_scope);
-    conf.service(auth_scope);
+    conf.service(scope);
 }
--- src/security/jwt.rs
+++ src/security/jwt.rs
@@ -22,29 +22,15 @@ pub fn create_jwt() -> Result<String, Box<dyn std::error::Error>> {
 }
 
 // pub fn decode_jwt(token: &str) -> Result<JWTClaims, Box<dyn std::error::Error>> {
+//     // Secret key used to decode the token
 //     let decoding_key = DecodingKey::from_secret("your-secret-key".as_ref());
 
-//     let validation = Validation {
-//         leeway: 0,
-//         validate_exp: true,
-//         ..Validation::default()
-//     };
+//     // Validation settings (e.g., check expiration)
+//     let validation = Validation::new(Algorithm::HS256);
 
-//     let token_data = decode::<JWTClaims>(&token, &decoding_key, &validation)?;
+//     // Decode the token and validate the claims
+//     let token_data: TokenData<JWTClaims> = decode::<JWTClaims>(&token, &decoding_key, &validation)?;
 
+//     // Return the decoded claims
 //     Ok(token_data.claims)
 // }
-
-pub fn decode_jwt(token: &str) -> Result<JWTClaims, Box<dyn std::error::Error>> {
-    // Secret key used to decode the token
-    let decoding_key = DecodingKey::from_secret("your-secret-key".as_ref());
-
-    // Validation settings (e.g., check expiration)
-    let validation = Validation::new(Algorithm::HS256);
-
-    // Decode the token and validate the claims
-    let token_data: TokenData<JWTClaims> = decode::<JWTClaims>(&token, &decoding_key, &validation)?;
-
-    // Return the decoded claims
-    Ok(token_data.claims)
-}
